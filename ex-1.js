2215641fc9714ccf8eb69a6b616845fe99f8de7a
Krittayapornn <krittayaporn.ww@gmail.com>
2024-07-17 14:46:08 +0700

    problem solving algorithm

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,32 @@
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+  let left = 0;
+  let right = students.length - 1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (students[mid] < searchStudent) {
+      left = mid + 1;
+	}
+    else if (students[mid] > searchStudent) {
+      right = mid - 1;
+    }else {
+		return mid;
+	}
+  }
+  return -1;
 }
 
+let students = [
+  "Andrew",
+  "Bobby",
+  "Diana",
+  "George",
+  "Hannah",
+  "Isaac",
+  "Jasmine",
+  "John",
+];
+let searchStudent = "Jasmine";
+
+console.log(findStudentIndex(students, searchStudent));
 // ตอบคำถามตรงนี้จ้า
+//Binary Search O(log(n)) ในการวน loop แต่ละครั้ง จะตัดตัวที่ไม่ต้องหาออกครึ่งหนึ่งของ array 
